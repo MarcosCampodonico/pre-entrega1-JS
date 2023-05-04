342e0a3f03cf2f84177054ae34339b775f27480a
MarcosCampodonico <marcoscampodonico4550@outlook.com>
2023-05-04 15:24:35 -0300

    Primer preentrega de js, cuando habras el codigo fijate que esta todo comentado para dejar todo en la misma pagina

---
+++ main.js
@@ -0,0 +1,44 @@
+//primer pre-entrega
+/*let tipoDeBien =prompt("Ingrese a que tipo de bien pertenece")
+let precio =parseInt(prompt("ingrese el precio del producto"))
+let imp=parseFloat(prompt("ingrese el porcentaje de impuestos que se le aplica al producto"))
+if(precio>1000 && tipoDeBien =="producto de lujo"){
+    let resultado= precio * imp
+    alert(`Precio final del producto: ${resultado}`)
+}else if(precio<1000 && tipoDeBien=="producto de consumo"){
+    let descuento = parseInt(prompt("ingrese el descuento"))
+    let resultado = precio - descuento
+    alert(`Precio final del producto: ${resultado}`)
+} else{
+    alert("el producto no esta disponible para la comercializacion")
+}*/
+
+
+/*let bien=prompt("tipo de producto")
+if(bien == "producto nacional"){
+    let pre=parseInt(prompt("ingrese el precio del producto"))
+    let descuento=parseInt(prompt("Ingrese el descuento que se le aplica al producto"))
+    let resultado = pre - descuento
+    alert(`El precio final del producto es: ${resultado}`)
+} else if(bien == "producto importado"){
+    let pre=parseInt(prompt("ingrese el precio del producto"))
+    let imp = parseFloat(prompt("ingrese el porcentaje de impuestos a aplicar"))
+    let resultado = pre * imp
+    alert(`El precio final del producto es: ${resultado}`)
+} else{
+    alert("Producto fuera de comercializacion")*/
+//}
+//ejercicio de ciclo for
+for(let i = 1;i<=10;i++){
+    let nombre1=prompt("ingrese su nombre")
+    let ape=prompt("ingrese su apellido")
+    let edad=parseInt(prompt("ingrese su edad"))
+    let genero=prompt("ingrese su genero")
+    if(genero =="femenino"){
+        alert(`srta. ${nombre1} ${ape} usted tiene el turno n° ${i} en el edificio F`)
+    }else if(genero=="masculino"){
+        alert(`sr. ${nombre1} ${ape} usted tiene el turno n° ${i} en el edificio M`)
+    } else{
+        alert("No tiene genero identificado")
+    }
+}
